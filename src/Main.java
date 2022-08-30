1d724531c4172a6d5dea513e4443e8b61fad63db
teinrus <Tnrkip@yandex.ru>
2022-08-30 13:36:23 +0300

    added lambda to Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 
 import java.util.*;
-import java.util.function.DoubleSupplier;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
